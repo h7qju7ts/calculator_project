3e042cc1526ebd12bc48e3dc2a631acc5c13d0ea
Adrian <h7qju7ts@students.codeinstitute.net>
2025-04-10 19:45:40 +0100

    Initial commit: Add calculator with add and subtract functions"

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    # add two numbers and return the result
+    return a + b
+def substract(a,b):
+    # substract b from a and return the result
+    return a - b
+
+# test our functions
+print(f"5 +3 = {add(5, 3)}")
+print(f"10 - 4 = {substract(10,4)}")
