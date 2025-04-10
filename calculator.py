97010cdf297f522a010fe51387353be62ad72e4f
Adrian <h7qju7ts@students.codeinstitute.net>
2025-04-10 19:54:43 +0100

    Add multiplication function

--- calculator.py
+++ calculator.py
@@ -8,3 +8,9 @@ def substract(a,b):
 # test our functions
 print(f"5 +3 = {add(5, 3)}")
 print(f"10 - 4 = {substract(10,4)}")
+
+def multiply(a, b):
+    # multiply two numbers and return the result
+    return a * b
+# test new function
+print(f"4 * 6 = {multiply(4, 6)}")
